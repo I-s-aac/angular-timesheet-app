46a05771e1e0df39c9799a577eba15d4462189c2
runaround <runaroundontheground@gmail.com>
2025-03-28 09:47:09 -0600

    unit 3, add toolbar, icon, and button, manually fix icon not centered in button

--- hr-timesheet/src/app/app.module.ts
+++ hr-timesheet/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { TimesheetComponent } from './components/timesheet/timesheet.component';
 import { AnalyticsComponent } from './components/analytics/analytics.component';
 import { TopNavbarComponent } from './components/top-navbar/top-navbar.component';
 import { AnalyticsTableComponent } from './components/analytics-table/analytics-table.component';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { AnalyticsTableComponent } from './components/analytics-table/analytics-
     TopNavbarComponent,
     AnalyticsTableComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
