033c46b64fc5a95bf1ceb628f17f7456eaf3ba8f
runaround <runaroundontheground@gmail.com>
2025-04-01 08:06:20 -0600

    unit 5, add department style, html, and get departments from service

--- hr-timesheet/src/app/components/departments/departments.component.ts
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
   selector: 'app-departments',
   standalone: false,
   templateUrl: './departments.component.html',
-  styleUrl: './departments.component.scss'
+  styleUrl: './departments.component.scss',
 })
-export class DepartmentsComponent {
-
+export class DepartmentsComponent implements OnInit {
+  private departmentsService = inject(DepartmentsService);
+  departments: Department[];
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+  }
 }
