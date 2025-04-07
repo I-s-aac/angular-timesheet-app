136ee80d5dacc614aafbc5d21e45d03849058813
runaround <runaroundontheground@gmail.com>
2025-04-07 08:33:38 -0600

    unit 7 part 2, add employee interface and addEmployee function to timesheet component with input and button

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
 import { FormControl } from '@angular/forms';
+import { Employee } from '../../interfaces/employee';
 
 @Component({
   selector: 'app-timesheet',
@@ -17,11 +18,27 @@ export class TimesheetComponent implements OnInit {
   departments: Department[];
   department: Department | undefined;
   employeeNameFC = new FormControl('');
+  employees: Employee[] = [];
+  employeeId: 0;
 
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find((department) => {
       return department.id === this.route.snapshot.params['id'];
     });
+  }
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department?.id,
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+    }
   }
 }
