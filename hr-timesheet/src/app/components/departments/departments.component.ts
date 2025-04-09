a246ef86f776ffd5f83229fd46e03581b62bd3bd
runaround <runaroundontheground@gmail.com>
2025-04-09 08:27:50 -0600

    use async pipe instead of previous

--- hr-timesheet/src/app/components/departments/departments.component.ts
+++ hr-timesheet/src/app/components/departments/departments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { DepartmentsService } from '../../services/departments.service';
 import { Department } from '../../interfaces/department';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-departments',
@@ -14,11 +15,13 @@ export class DepartmentsComponent implements OnInit {
   private router = inject(Router);
 
   departments: Department[];
+  $departments: Observable<Department[]> | undefined;
 
   ngOnInit(): void {
-    this.departmentsService.getDepartments().subscribe((departments) => {
-      this.departments = departments;
-    });
+    // this.departmentsService.getDepartments().subscribe((departments) => {
+    //   this.departments = departments;
+    // });
+    this.$departments = this.departmentsService.getDepartments();
   }
 
   goToDepartment(departmentId: string): void {
